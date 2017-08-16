ae9935c678c8ca64dea69fa9341c8e9b32d9c7ad
Bruce Tsvil <superunknown1103@gmail.com>
2017-08-16 08:42:11 -0500

    connection.js

---
+++ app/config/connection.js
@@ -0,0 +1,24 @@
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('library', 'root',
+'Superman1103', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 1000
+  }
+});
+
+// sequelize authentication
+sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+module.exports = sequelize;
